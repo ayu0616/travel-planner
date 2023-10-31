565cadc0c3a7fc2bc40e196c1e7ef19b0799fa0f
ayu0616 <ayumuogawa0616@gmail.com>
2023-10-31 23:14:23 +0900

    スコアを定義した

---
+++ src/define_score.cpp
@@ -0,0 +1,115 @@
+// スコアの定義を指定してみる
+
+#include "../tp-lib.hpp"
+
+int N;
+vector<Place> places;
+
+struct State : public StateBase
+{
+    // スコアは高いほうが良い
+    double score() const override
+    {
+        int s = 0;
+        rep(i, N)
+        {
+            if (visited >> i & 1)
+                s += places[i].priority;
+        }
+        return s;
+    }
+};
+
+int main()
+{
+    cin >> N;
+    vector<vector<double>> A(N, vector<double>(N));
+    rep(i, N) rep(j, N) cin >> A[i][j];
+
+    places.resize(N);
+    rep(i, N)
+    {
+        places[i].id = i;
+        int ab, aa, st, pr;
+        cin >> ab >> aa >> st >> pr;
+        if (ab != -1)
+            places[i].arrive_before = ab;
+        if (aa != -1)
+            places[i].arrive_after = aa;
+        places[i].stay_time = st;
+        places[i].priority = pr;
+    }
+
+    // 初期状態
+    State *init = new State();
+    init->place = &places[0];
+    init->prev = nullptr;
+    init->arrive_at = 0;
+    init->visited = 0;
+
+    // ゴール判定
+    auto is_goal = [&](State *s)
+    {
+        return s->visited == (1 << N) - 1;
+    };
+
+    priority_queue<State *,
+                   vector<State *>,
+                   function<bool(State *, State *)>>
+        que([](State *s, State *t)
+            { return *s < *t; }); // スコアが高い順に取り出す
+    que.push(init);
+
+    State *ans = nullptr;
+    while (!que.empty())
+    {
+        State *s = que.top();
+        que.pop();
+        Place cp = *s->place;
+        if (is_goal(s) && (ans == nullptr || *s > *ans)) // スコアが高ければ解を更新
+        {
+            ans = s;
+        }
+        if (s->arrive_at + cp.stay_time + A[cp.id][0] > places[0].arrive_before)
+        {
+            auto tmp = new State();
+            auto prev = s->prev;
+            tmp->place = &places[0];
+            tmp->prev = prev;
+            tmp->arrive_at = prev->arrive_at + prev->place->stay_time + A[prev->place->id][0];
+            tmp->visited = (s->visited ^ (1 << cp.id)) | (1 << 0);
+            if (ans == nullptr || *tmp > *ans)
+            {
+                ans = tmp;
+            }
+            continue;
+        }
+
+        rep(i, N)
+        {
+            if (s->visited & (1 << i) || (i == 0 && s->visited_len() != N - 1))
+                continue;
+            double arrive_at = s->arrive_at + cp.stay_time + A[cp.id][i];
+            if (!places[i].is_visitable(arrive_at))
+                continue;
+            State *next = new State();
+            next->place = &places[i];
+            next->prev = s;
+            next->arrive_at = arrive_at;
+            next->visited = s->visited | (1 << i);
+            que.push(next);
+        }
+    }
+
+    if (ans)
+    {
+        auto visited_places = ans->trace_back();
+
+        cout << ans->arrive_at << endl;
+        cout << visited_places << endl;
+    }
+    else
+    {
+        cout << "-1" << endl;
+    }
+}
--- tp-lib.hpp
+++ tp-lib.hpp
@@ -29,6 +29,8 @@ struct Place
     ll arrive_after;
     // 滞在時間（分）
     ll stay_time;
+    // 行きたい度
+    int priority;
 
     // @brief idで初期化
     // @param id 観光スポットのid
@@ -38,6 +40,7 @@ struct Place
         arrive_before = 24 * 60 - 1;
         arrive_after = 0;
         stay_time = 0;
+        priority = 1;
     };
 
     // @brief 訪問可能かどうかを判定する
@@ -49,6 +52,11 @@ struct Place
     };
 };
 
+struct StateBase;
+
+template <typename T>
+concept StateBaseType = is_base_of<StateBase, T>::value;
+
 /**
  * 状態を管理する構造体
  * それぞれの実装で継承して使う
@@ -58,19 +66,65 @@ struct StateBase
     Place *place;
     StateBase *prev;
     double arrive_at;
+    ll visited;
 
     StateBase()
     {
         place = nullptr;
         prev = nullptr;
         arrive_at = 0;
+        visited = 0;
     };
+
+    // 前に訪れたスポットを辿っていく関数
+    vector<StateBase *> trace_back()
+    {
+        vector<StateBase *> visited_places;
+        auto current_place = this;
+        while (current_place->prev != nullptr)
+        {
+            visited_places.push_back(current_place);
+            current_place = current_place->prev;
+        }
+        visited_places.push_back(current_place);
+        reverse(all(visited_places));
+        return visited_places;
+    }
+
+    int visited_len() const
+    {
+        return popcount((unsigned ll)visited);
+    }
+
+    // スコア関数
+    virtual double score() const = 0;
 };
 
-// 前に訪れたスポットを辿っていく関数
-vector<StateBase *> trace_back(StateBase *current_place)
+template <StateBaseType T>
+bool operator<(const T &l, const T &r)
 {
-    vector<StateBase *> visited_places;
+    return l.score() < r.score();
+};
+template <StateBaseType T>
+bool operator>(const T &l, const T &r)
+{
+    return r < l;
+};
+template <StateBaseType T>
+bool operator<=(const T &l, const T &r)
+{
+    return !(l > r);
+};
+template <StateBaseType T>
+bool operator>=(const T &l, const T &r)
+{
+    return !(l < r);
+};
+
+template <StateBaseType T>
+vector<T *> trace_back(T *current_place)
+{
+    vector<T *> visited_places;
     while (current_place->prev != nullptr)
     {
         visited_places.push_back(current_place);
@@ -81,7 +135,8 @@ vector<StateBase *> trace_back(StateBase *current_place)
     return visited_places;
 }
 
-ostream &operator<<(ostream &os, const vector<StateBase *> &vp)
+template <StateBaseType T>
+ostream &operator<<(ostream &os, const vector<T *> &vp)
 {
     for (auto p : vp)
         os << p->place->id << " " << p->arrive_at << endl;
