b93e28b9891c741d207f6895c824e4d07a8db06c
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-16 08:15:09 +0900

    feat: variant: secondaryを追加した

--- frontend/src/components/atoms/Button/Button.tsx
+++ frontend/src/components/atoms/Button/Button.tsx
@@ -6,15 +6,17 @@ interface ButtonProps
         HTMLButtonElement
     > {
     /** 見た目の種類 */
-    variant?: 'normal' | 'outline'
+    variant?: 'normal' | 'outline' | 'secondary'
 }
 
 const variantToClass = (variant: ButtonProps['variant']) => {
     switch (variant) {
         case 'normal':
-            return 'bg-green-700 text-white hover:bg-green-800 active:bg-green-900'
+            return 'bg-emerald-700 text-white hover:bg-emerald-800 active:bg-emerald-900 focus:ring-emerald-800/50'
         case 'outline':
-            return 'bg-transparent text-green-700 outline outline-2 -outline-offset-2 outline-green-700 hover:bg-green-800 hover:text-white hover:outline-green-800 active:bg-green-900 active:text-white'
+            return 'bg-transparent text-emerald-700 outline outline-2 -outline-offset-2 outline-emerald-700 hover:bg-emerald-800 hover:text-white hover:outline-emerald-800 active:bg-emerald-900 active:text-white focus:ring-emerald-800/50'
+        case 'secondary':
+            return 'bg-slate-500 text-white hover:bg-slate-500/80 active:bg-slate-500/50 focus:ring-slate-800/50'
         default:
             return ''
     }
@@ -29,7 +31,7 @@ const Button = ({
     return (
         <button
             className={arrToClass(
-                'rounded-md px-3 py-2 focus:ring-2 focus:ring-green-800/50',
+                'rounded-md px-3 py-2 focus:ring-2',
                 variantClass,
                 className,
             )}
