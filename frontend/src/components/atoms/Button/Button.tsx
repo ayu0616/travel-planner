63691daf5e7f8f2162135f7e9e49a5fe88d130b5
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-16 00:30:27 +0900

    feat: outlineを追加した

--- frontend/src/components/atoms/Button/Button.tsx
+++ frontend/src/components/atoms/Button/Button.tsx
@@ -4,13 +4,33 @@ interface ButtonProps
     extends React.DetailedHTMLProps<
         React.ButtonHTMLAttributes<HTMLButtonElement>,
         HTMLButtonElement
-    > {}
+    > {
+    /** 見た目の種類 */
+    variant?: 'normal' | 'outline'
+}
 
-const Button = ({ className = '', ...props }: ButtonProps) => {
+const variantToClass = (variant: ButtonProps['variant']) => {
+    switch (variant) {
+        case 'normal':
+            return 'bg-green-700 text-white hover:bg-green-800 active:bg-green-900'
+        case 'outline':
+            return 'bg-transparent text-green-700 outline outline-2 -outline-offset-2 outline-green-700 hover:bg-green-800 hover:text-white hover:outline-green-800 active:bg-green-900 active:text-white'
+        default:
+            return ''
+    }
+}
+
+const Button = ({
+    variant = 'normal',
+    className = '',
+    ...props
+}: ButtonProps) => {
+    const variantClass = variantToClass(variant)
     return (
         <button
             className={arrToClass(
-                'rounded-md bg-green-700 px-3 py-2 text-white outline-2 outline-green-800/50 hover:bg-green-800 focus:outline active:bg-green-900',
+                'rounded-md px-3 py-2 focus:ring-2 focus:ring-green-800/50',
+                variantClass,
                 className,
             )}
             {...props}
