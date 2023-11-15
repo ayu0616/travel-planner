6662bc3cdbbd3b20b462eb180ee06859a2781f4c
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-15 09:22:47 +0900

    add: storybookでtailwindを見られるように設定を追加した

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,15 +1,18 @@
 import type { Preview } from "@storybook/react";
 
+import "tailwindcss/tailwind.css";
+import "../src/index.css";
+
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        actions: { argTypesRegex: "^on[A-Z].*" },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 };
 
 export default preview;
