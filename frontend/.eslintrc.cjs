1731af3291ca302306b736e885f5dc9c05bf94ad
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-15 12:44:13 +0900

    feat: eslintを修正した

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,12 +1,7 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:storybook/recommended"
-    ],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
     ignorePatterns: ["dist", ".eslintrc.cjs"],
     parser: "@typescript-eslint/parser",
     plugins: ["react-refresh"],
@@ -18,5 +13,6 @@ module.exports = {
     },
     rules: {
         "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+        "@typescript-eslint/no-unused-vars": "warn",
     },
 };
