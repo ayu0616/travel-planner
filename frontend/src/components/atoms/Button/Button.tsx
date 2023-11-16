6cb47d3c42b4a14aa0cab9d0c7588071d64b1775
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-16 18:29:50 +0900

    fix: ring -> outline

--- frontend/src/components/atoms/Button/Button.tsx
+++ frontend/src/components/atoms/Button/Button.tsx
@@ -12,11 +12,11 @@ interface ButtonProps
 const variantToClass = (variant: ButtonProps['variant']) => {
     switch (variant) {
         case 'normal':
-            return 'bg-emerald-700 text-white hover:bg-emerald-800 active:bg-emerald-900 focus:ring-emerald-800/50'
+            return 'bg-emerald-700 text-white hover:bg-emerald-800 active:bg-emerald-900 focus:outline-emerald-800/50'
         case 'outline':
-            return 'bg-transparent text-emerald-700 outline outline-2 -outline-offset-2 outline-emerald-700 hover:bg-emerald-800 hover:text-white hover:outline-emerald-800 active:bg-emerald-900 active:text-white focus:ring-emerald-800/50'
+            return 'bg-transparent text-emerald-700 outline outline-2 -outline-offset-2 outline-emerald-700 hover:bg-emerald-800 hover:text-white hover:outline-emerald-800 active:bg-emerald-900 active:text-white focus:outline-emerald-800/50'
         case 'secondary':
-            return 'bg-slate-500 text-white hover:bg-slate-500/80 active:bg-slate-500/50 focus:ring-slate-800/50'
+            return 'bg-slate-500 text-white hover:bg-slate-500/80 active:bg-slate-500/50 focus:outline-slate-800/50'
         default:
             return ''
     }
@@ -31,7 +31,7 @@ const Button = ({
     return (
         <button
             className={arrToClass(
-                'w-full rounded-md px-3 py-2 focus:ring-2',
+                'w-full rounded-md px-3 py-2 focus:outline focus:outline-2',
                 variantClass,
                 className,
             )}
