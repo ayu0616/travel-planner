4906a86356823e17ff249b431ca1a997e0358f41
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-16 13:34:38 +0900

    add: w-fullを追加した

--- frontend/src/components/atoms/Button/Button.tsx
+++ frontend/src/components/atoms/Button/Button.tsx
@@ -31,7 +31,7 @@ const Button = ({
     return (
         <button
             className={arrToClass(
-                'rounded-md px-3 py-2 focus:ring-2',
+                'w-full rounded-md px-3 py-2 focus:ring-2',
                 variantClass,
                 className,
             )}
