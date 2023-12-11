3f34202e76fd19fd0eea897a07c6cc864a9f49f7
ayu0616 <ayumuogawa0616@gmail.com>
2023-12-11 20:36:43 +0900

    開発環境用にdockerの設定をした

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +11,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
