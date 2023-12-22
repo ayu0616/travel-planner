431561fd135476aaa04861c5ef4f930f91f4f5ec
ayu0616 <ayumuogawa0616@gmail.com>
2023-12-22 09:55:09 +0900

    スコア計算を関数化した

--- backend/python/test.py
+++ backend/python/test.py
@@ -10,22 +10,30 @@ from multiprocessing import freeze_support
 os.chdir(os.path.dirname(__file__))
 
 in_files = glob.glob(os.path.join("../in", "*.txt"))
-EXEC_FILE = "../dist/sa"
 
-score = 0.0
+scores = [0.0] * len(in_files)
+EXEC_FILE = "../dist/main"
+
+
+def get_scores(exec_file: str):
+    global EXEC_FILE, scores
+    EXEC_FILE = exec_file
+    scores = [0.0] * len(in_files)
+    freeze_support()
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        executor.map(exec_cpp, in_files)
+    return scores
 
 
 def exec_cpp(in_file: str):
     res = subprocess.run(f"{EXEC_FILE} < {in_file}", shell=True, stdout=subprocess.PIPE)
     s = float(res.stdout.decode().split("\n")[0])
-    global score
-    score += s
+    global scores
+    scores[int(os.path.basename(in_file).split(".")[0])] = s
     # print(os.path.basename(in_file), s)
 
 
 if __name__ == "__main__":
-    freeze_support()
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        executor.map(exec_cpp, in_files)
+    s = sum(get_scores(EXEC_FILE))
 
-    print(EXEC_FILE, score)
+    print(EXEC_FILE, s)
